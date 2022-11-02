823a12a912a94ff542cfe15dad20cc79b29db647
MarceloYudis <marceloyudisortiz@gmail.com>
2022-11-02 20:16:05 -0300

    arreglo de mi codigo. Uso de generator para extraer datos de una api

--- gen/fetchWithGen.js
+++ gen/fetchWithGen.js
@@ -1,27 +1,33 @@
 import fetch from 'node-fetch';
 const API = 'https://api.escuelajs.co/api/v1';
 
-async function* fatherGenerator(urlApi) {
+export default async function fatherGenerator(urlApi) {
     const info = await fetch(urlApi);
     const infoToJson = await info.json();
-    yield  infoToJson;
+    return infoToJson;
 }
 
-async function obtencionEnConsola(apiUrl) {
-    const firstInfo = await fatherGenerator(`${apiUrl}/products`).next().value;
+async function* obtencionEnConsola(apiUrl) {
+    try{
+        const firstInfo = await fatherGenerator(`${apiUrl}/products`);
     const prroducts = firstInfo;
-    console.log(prroducts);
+    console.log(prroducts[107]);
 
     const oneProduct = await fatherGenerator(`${apiUrl}/products/${prroducts[0].id}`);
-    console.log(oneProduct.next().value);
+    console.log(oneProduct.title);
 
-    const categories = await fatherGenerator(`${apiUrl}/categories/${product.category.id}`);
-    console.log(categories.next().value)
+    const categories = await fatherGenerator(`${apiUrl}/categories/${oneProduct.category.id}`);
+    console.log(categories.name)
 
+    }catch (error) {
+        console.log('eRRoR'. error)
+    }
     
    
     
 }
 
-obtencionEnConsola(API)
+const result = obtencionEnConsola(API);
+result.next();
+result.next();
 
