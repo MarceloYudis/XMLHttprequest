8d435582857a9d88c8747d5e3a8361a77dbd8c0a
MarceloYudis <marceloyudisortiz@gmail.com>
2022-11-02 19:49:26 -0300

    Probando generadores y fetch

--- gen/fetchWithGen.js
+++ gen/fetchWithGen.js
@@ -4,21 +4,23 @@ const API = 'https://api.escuelajs.co/api/v1';
 function* fatherGenerator(urlApi) {
     const info = fetch(urlApi);
     const infoToJson = info.json();
-    yield  infoToJson;
+    yield  infoToJson();
 }
 
 function obtencionEnConsola(apiUrl) {
-    const firstInfo = fatherGenerator(`${urlApi}/products`);
+    const firstInfo = fatherGenerator(`${apiUrl}/products`);
     console.log(firstInfo.next().value);
 
-    const oneProduct = fatherGenerator(`${urlApi}/products/${products[0].id}`);
+    const oneProduct = fatherGenerator(`${apiUrl}/products/${products[0].id}`);
     console.log(oneProduct.next().value);
 
-    const categories = fatherGenerator(`${urlApi}/categories/${product.category.id}`);
+    const categories = fatherGenerator(`${apiUrl}/categories/${product.category.id}`);
     console.log(categories.next().value)
 
     
    
     
 }
+
+obtencionEnConsola(API)
 
--- gen/fetchWithGenResolve.js
+++ gen/fetchWithGenResolve.js
@@ -2,5 +2,25 @@ import fetch from 'node-fetch';
 const API = 'https://api.escuelajs.co/api/v1';
 
 async function* fatherGenerator(urlApi) {
-    
+    const response = await fetch(urlApi);
+    yield await response.json()
 }
+
+function anotherFunction(apiUrl) {
+
+    fatherGenerator(`${apiUrl}/products`).next().then( ({value,done}) => {
+        console.log(value);
+        const product = value[0].id;
+
+        fatherGenerator(`${apiUrl}/products/${product}`).next().then( ({value, done}) => {
+            console.log(value.title)
+            const categorY = value.category.id;
+
+            fatherGenerator(`${urlApi}/categories/${categorY}`).next().then( (value,done)=>{
+                console.log(value.name)
+            })
+        })
+    })
+}
+
+anotherFunction(API)
